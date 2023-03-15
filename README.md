4b892dd31c649f8312fe8667fd8e961ee022e306
Arojo <rojoserrano@gmail.com>
2023-03-15 11:49:45 +0100

    First commit for app deployment in streamlit

---
+++ README.md
@@ -0,0 +1,82 @@
+Pagoda
+==============================
+
+A short description of the project.
+
+## Development Environment Setup
+
+1. Ensure you have Docker install on your machine
+2. Under the project folder run `docker-compose up --build --detach` in terminal to build the project
+3. Open `http://pagoda.localhost` on your browser and enjoy!
+
+### Start / Stop project:
+You don't need to build your project all times:
+- To start project run `docker-compose start pagoda`
+- To stop project run `docker-compose stop pagoda`
+
+Project Organization
+------------
+
+    ├── LICENSE
+    ├── Makefile           <- Makefile with commands like `make data` or `make train`
+    ├── README.md          <- The top-level README for developers using this project.
+    ├── data
+    │   ├── external       <- Data from third party sources.
+    │   ├── interim        <- Intermediate data that has been transformed.
+    │   ├── processed      <- The final, canonical data sets for modeling.
+    │   └── raw            <- The original, immutable data dump.
+    │
+    ├── docs               <- A default Sphinx project; see sphinx-doc.org for details
+    │
+    ├── models             <- Trained and serialized models, model predictions, or model summaries
+    │
+    ├── notebooks          <- Jupyter notebooks. Naming convention is a number (for ordering),
+    │                         the creator's initials, and a short `-` delimited description, e.g.
+    │                         `1.0-jqp-initial-data-exploration`.
+    │
+    ├── references         <- Data dictionaries, manuals, and all other explanatory materials.
+    │
+    ├── reports            <- Generated analysis as HTML, PDF, LaTeX, etc.
+    │   └── figures        <- Generated graphics and figures to be used in reporting
+    │
+    ├── requirements.txt   <- The requirements file for reproducing the analysis environment, e.g.
+    │                         generated with `pip freeze > requirements.txt`
+    │
+    ├── setup.py           <- makes project pip installable (pip install -e .) so src can be imported
+    ├── src                <- Source code for use in this project.
+    │   ├── __init__.py    <- Makes src a Python module
+    │   │
+    │   ├── data           <- Scripts to download or generate data
+    │   │   └── make_dataset.py
+    │   │
+    │   ├── features       <- Scripts to turn raw data into features for modeling
+    │   │   └── build_features.py
+    │   │
+    │   ├── models         <- Scripts to train models and then use trained models to make
+    │   │   │                 predictions
+    │   │   ├── predict_model.py
+    │   │   └── train_model.py
+    │   │
+    │   └── visualization  <- Scripts to create exploratory and results oriented visualizations
+    │       └── visualize.py
+    │
+    └── tox.ini            <- tox file with settings for running tox; see tox.readthedocs.io
+
+
+--------
+
+<p><small>Project based on the <a target="_blank" href="https://drivendata.github.io/cookiecutter-data-science/">cookiecutter data science project template</a>. #cookiecutterdatascience</small></p>
+
+
+# TODO
+1. Improve Best Practise for Development & Production envs, Flask debug mode should be set to True/False accordingly
+2. 
+
+
+# AGENDA
+
+## DAY 2
+
+### Agustin
+
+Explore the data further to get interactions between users. Created a csv file with list of users and the users they follow. Saved it on follows.csv
---
+++ near_social_segment_app.py
@@ -0,0 +1,239 @@
+# Import
+
+import pandas as pd
+import numpy as np
+import streamlit as st
+import json
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+
+import plotly.io as pio
+from datetime import datetime, timedelta
+
+# Streamlit
+
+# Set st title and header
+st.set_page_config(page_title='Near Social Segmentation Tool',
+                   page_icon='random',
+                   layout='wide')
+st.title('Near Social Segmentation Tool')
+
+# Define functions with caching
+@st.cache_data # decorator to add caching
+def load_data(csv):
+    df = pd.read_csv(csv)
+    df['first_social_sign'] = df['first_social_sign'].astype('datetime64[ns]')
+    return df.copy(deep=True)
+
+@st.cache_data
+def select_data(df, column, begin, end):
+    return df[(df[column]>= np.datetime64(begin)) & (df[column]<= np.datetime64(end))]
+
+@st.cache_data
+def agg_by_column(df, column):
+    return df.groupby(column).count().reset_index()
+
+## Dataframe for global variables - all-time signers
+df1 = load_data('data/auths_raw_df.csv')
+
+df1_agg = agg_by_column(df1[['user', 'first_social_sign']],'first_social_sign').rename({'first_social_sign': 'Date', 'user':'Daily Users'}, axis=1)
+
+since = np.min(df1_agg['Date'])
+last_updated = np.max(df1_agg['Date'])
+total_users = np.max(df1_agg['Daily Users'].cumsum())
+
+# Set st sidebar
+with st.sidebar:
+    st.write("Analysed Timeframe")
+
+    timeframe = st.radio('Select timeframe:',
+                        ('Last 30 days', 'Last 60 days', 'Last 90 days', 'Custom timeframe', 'All-time'),
+                        index=4
+                        )
+
+    if timeframe == 'Last 30 days':
+        begin_date = datetime.today() - timedelta(days=30) - timedelta(days=1)
+        end_date = datetime.today() - timedelta(days=1)
+        st.write('Selected timeframe is last 30 days')
+
+    if timeframe == 'Last 60 days':
+        begin_date = datetime.today() - timedelta(days=60) - timedelta(days=1)
+        end_date = datetime.today() - timedelta(days=1)
+        st.write('Selected timeframe is last 60 days')
+
+    if timeframe == 'Last 90 days':
+        begin_date = datetime.today() - timedelta(days=90) - timedelta(days=1)
+        end_date = datetime.today() - timedelta(days=1)
+        st.write('Selected timeframe is last 90 days')
+
+    if timeframe == 'Custom timeframe':
+        col1, col2, col3 = st.columns(3)
+
+        begin_date = st.date_input('Select begin date:',
+                                    value=datetime.today()- timedelta(days=91),
+                                    #min_value=datetime.date(2022,2,16),
+                                    max_value=datetime.today()- timedelta(days=1)
+                                )
+        end_date = st.date_input('Select end date:',
+                                    value=datetime.today()- timedelta(days=1),
+                                    min_value=begin_date,
+                                    max_value=datetime.today()- timedelta(days=1)
+                                )
+
+        if begin_date != end_date:
+            st.write(f'Selected timeframe is between {begin_date} and {end_date}')
+        else:
+            st.write(f'Selected timeframe is on {begin_date}')
+
+    if timeframe == 'All-time':
+        begin_date = since
+        end_date = last_updated
+        st.write(f'Selected timeframe is between {str(begin_date)[:11]} and {str(end_date)[:11]}')
+
+# Load Filtered Dataframes
+
+## Dataframe for wallet age histogram and metrics
+df2 = select_data(df1, 'first_social_sign' , begin_date, end_date)
+
+## Dataframe for wallet age category pie chart
+df3 = agg_by_column(df2[['user', 'age_category']], 'age_category').rename({'user': 'wallets'}, axis=1)
+
+## Dataframe for human-readable addresses pie 
+df4 = agg_by_column(df2[['user', 'human_readable']], 'human_readable').rename({'user': 'wallets'}, axis=1)
+
+## Dataframe for swaps 
+df5 = load_data('data/swaps_raw_df.csv')
+
+## Dataframe for signers - selected timeframe wallet age and social activity
+#df3 = df2[(df2['Date']>= np.datetime64(begin_date)) & (df2['Date']<= np.datetime64(end_date))]
+#df3
+
+# Set st tabs
+tabs = st.tabs(["Introduction", "Wallet age and address type", "Social.NEAR activity", "DeFi", "NFT"])
+
+with tabs[0]:
+    st.write('''Social.NEAR is an on-chain social network built on the NEAR blockchain that has recently seen a surge 
+    in the number of users signing in for the first time. This dashboard presents a tool to analyze the new user segmentation 
+    for a given timeframe. To operate it, select a timeframe on the left sidebar and navigate through the tabs to analyze different profile categories.
+    ''')
+
+    cols = st.columns([1,2,2,2,1])
+    cols[1].metric("Total users", total_users, help='First-time signers of social.near contract')
+    cols[2].metric("Available data since", str(since)[:11], help='On-chain data first record')
+    cols[3].metric("Data last updated on", str(last_updated)[:11], help='On-chain data last record')
+
+    # Plot all-time signers
+    fig = make_subplots(specs=[[{'secondary_y': True}]])
+    
+    fig.add_trace(
+                go.Scatter(x=df1_agg['Date'], y=df1_agg['Daily Users'].cumsum(), name='Cumulative'), 
+                secondary_y=False
+                )
+
+    fig.add_trace(
+                go.Bar(x=df1_agg['Date'], y=df1_agg['Daily Users'], name='Daily'),    
+                secondary_y=True
+                )
+
+    fig.update_xaxes(title_text="<b>Date</b>")
+
+    fig.update_yaxes(title_text="<b>Cumulative Users</b> ", secondary_y=False)
+
+    fig.update_yaxes(title_text="<b>Daily Users</b> ", secondary_y=True)
+
+    fig.update_layout(
+                    title='<b>All-time New Users Signing to the Social.NEAR Contract</b>',
+                    autosize=False,
+                    width=900,
+                    height=600,
+                    margin=dict(
+                        l=100,
+                        r=100,
+                        b=100,
+                        t=100,
+                        pad=4
+                    ),
+                    paper_bgcolor="white",
+                    hovermode='x unified'
+                    )
+
+    st.plotly_chart(fig, use_container_width=True)
+    
+    st.markdown('''For more info on Social.Near visit http://near.social or 
+    this wonderful [article on NEAR wiki](https://thewiki.near.page/PastPresentAndFutureOfNearSocial)''')
+
+with tabs[1]:
+    # Wallet age at sign in plot
+    st.write('''This tab analyses the wallet age when signing to social.near contract for the first time and 
+    whether the addresses are human-readable or not (64 alphanumeric characters)''')
+
+    cols = st.columns([1,2,2,2,2,1])
+    cols[1].metric("Total users in timeframe", df2['age_days'].size, help='First-time signers of social.near contract in selected timeframe')
+    cols[2].metric("Wallet average age", round(df2['age_days'].mean(),2), help='Average wallet age at first sign-in')
+    cols[3].metric("Wallet mode age", round(df2['age_days'].mode(),2), help='Mode wallet age at first sign-in')
+    cols[4].metric("Same day signers", df2['age_days'][df2['age_days']==0].size, help='Wallets signing to Near Social the same day they were created')
+
+    with st.container():
+        cols = st.columns(2)
+
+        with cols[1]:
+            fig = px.pie(df4,
+                        values='wallets',
+                        names='human_readable',
+                        title='Human readable addresses')
+
+            fig.update_traces(textinfo='percent+label')
+
+            fig.update(layout_showlegend=False)
+
+            st.plotly_chart(fig, use_container_width=True)
+
+        with cols[0]:
+            fig = px.pie(df3,
+                        values='wallets',
+                        names='age_category',
+                        title='Wallets by age category')
+
+            fig.update_traces(textinfo='percent+label')
+
+            fig.update(layout_showlegend=False)
+
+            st.plotly_chart(fig, use_container_width=True)
+
+    with st.container():
+        fig = px.histogram(df2, 
+                        x='age_days',
+                        color='human_readable',
+                        #marginal='box',
+                        nbins=20)
+
+        fig.update_layout(bargap=0.1,
+                        title='Distribution of users by wallet age in days when signing in Social.NEAR for the first time',
+                        font=dict(color='black'
+                                ),
+                        hoverlabel=dict(bgcolor='white',
+                                        font_size=12
+                                        )
+                        )
+
+        fig.update_xaxes(title='Wallet Age [days]',
+                        title_font=dict(size=14,
+                                        color='black'
+                                        )
+                        )
+
+        fig.update_yaxes(title='Number of Wallets',
+                        title_font=dict(size=14, 
+                                        color='black'
+                                        )
+                        )
+
+        #fig.update_traces(hovertemplate =
+        #                '<i>Age</i>: ' + '%{x} days<br>' +
+        #                '<i>Count</i>: ' + '%{y} wallets' + '<extra></extra>',
+        #
+        #                selector=dict(type="histogram"))
+
+        st.plotly_chart(fig, use_container_width=True)
+
